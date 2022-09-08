3d17816c02094c50223073ec507e9aa8541ff1cb
Bustanil Arifin <bustanil@protonmail.com>
2022-09-08 11:21:07 +0800

    use eip and scale up aws instances

--- prod.tf
+++ prod.tf
@@ -42,6 +42,8 @@ resource "aws_security_group" "prod_web" {
 }
 
 resource "aws_instance" "prod_web" {
+  count = 2
+
   ami = "ami-088404470e1d55992"
   instance_type = "t2.nano"
   
@@ -49,6 +51,17 @@ resource "aws_instance" "prod_web" {
     aws_security_group.prod_web.id
   ]
 
+  tags = {
+    "Terraform": "true"
+  }
+}
+
+resource "aws_eip_association" "prod_web" {
+  instance_id = aws_instance.prod_web[0].id
+  allocation_id = aws_eip.prod_web.id
+}
+
+resource "aws_eip" "prod_web" {
   tags = {
     "Terraform": "true"
   }
